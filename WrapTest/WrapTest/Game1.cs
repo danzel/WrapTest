f0d43d53f622af63a7105f76bc2ce990bfa832e5
David Leaver <danzel@localhost.geek.nz>
2014-05-04 21:20:00 +1200

    PointClamp test for mono/MonoGame#1114

--- WrapTest/WrapTest/Game1.cs
+++ WrapTest/WrapTest/Game1.cs
@@ -1,21 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-using Color = Microsoft.Xna.Framework.Color;
-using File = System.IO.File;
-using Matrix = Microsoft.Xna.Framework.Matrix;
-using Rectangle = Microsoft.Xna.Framework.Rectangle;
-
 #if ANDROID
 using Android.Graphics;
 using Java.Nio;
@@ -24,14 +9,6 @@ using Java.Nio;
 #if IOS
 using MonoTouch.UIKit;
 using MonoTouch.CoreGraphics;
-#endif
-
-#if WINDOWS
-using System.Drawing.Imaging;
-#endif
-
-#if TOUCH
-using Microsoft.Xna.Framework.Input.Touch;
 #endif
 
 namespace WrapTest
@@ -44,8 +21,6 @@ namespace WrapTest
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 
-		private int _mode = 1;
-		private Texture2D _checkers60;
 		private Texture2D _checkers64;
 
 		public Game1()
@@ -79,7 +54,6 @@ namespace WrapTest
 			spriteBatch = new SpriteBatch(GraphicsDevice);
 
 			// TODO: use this.Content to load your game content here
-			_checkers60 = Content.Load<Texture2D>("Checkers60");
 			_checkers64 = Content.Load<Texture2D>("Checkers64");
 		}
 
@@ -92,8 +66,6 @@ namespace WrapTest
 			// TODO: Unload any non ContentManager content here
 		}
 
-		bool _spaceWasPressed;
-
 		/// <summary>
 		/// Allows the game to run logic such as updating the world,
 		/// checking for collisions, gathering input, and playing audio.
@@ -101,153 +73,31 @@ namespace WrapTest
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update(GameTime gameTime)
 		{
-#if !PSS
 			// Allows the game to exit
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
 				this.Exit();
-			
-#if TOUCH
-			if (TouchPanel.GetState().Any(t => t.State == TouchLocationState.Pressed))
-				_mode = (_mode + 1) % 4;
-#else
-			var spacePressed = Keyboard.GetState().IsKeyDown(Keys.Space);
-
-			if (spacePressed && !_spaceWasPressed)
-			{
-				_mode = (_mode + 1) % 4;
-			}
-
-			_spaceWasPressed = spacePressed;
-#endif
-#endif
 			// TODO: Add your update logic here
 
 			base.Update(gameTime);
 		}
-		
-		DateTime _firstDrawTime;
-		int _drawCount = 0;
-		
-		double _first60fps;
+
 		/// <summary>
 		/// This is called when the game should draw itself.
 		/// </summary>
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Draw(GameTime gameTime)
 		{
-			if (_drawCount == 0)
-				_firstDrawTime = DateTime.Now;
-			_drawCount ++;
-			if (_drawCount == 60)
-			{
-				DateTime end = DateTime.Now;
-				TimeSpan timeTaken = end - _firstDrawTime;
-				_first60fps = 60 / timeTaken.TotalSeconds;
-				
-				_firstDrawTime = end;
-			}
-			if (_drawCount == 120)
-			{
-				DateTime end = DateTime.Now;
-				TimeSpan timeTaken = end - _firstDrawTime;
-				
-				var first = _first60fps;
-				var second = 60 / timeTaken.TotalSeconds;
-			}
-			
 			GraphicsDevice.Clear(Color.CornflowerBlue);
 
-			Texture2D texture = (_mode % 2 == 0) ? _checkers60 : _checkers64;
+			Texture2D texture =  _checkers64;
 			
-			//for (int i = 0; i < 10; i++)
-			{
-				if (_mode < 2) //Without matrix
-					spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.LinearWrap, null, null);
-				else //With matrix to rotate whole screen 180
-					spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.LinearWrap, null, null, null, Matrix.CreateRotationZ(MathHelper.ToRadians(180)) * Matrix.CreateTranslation(480, 320, 0));
+			spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, null, null);
 	
-				var size = new Vector2(texture.Width, texture.Height);
+			spriteBatch.Draw(texture, new Vector2(5, 5), null, Color.White, 0, Vector2.Zero, 4, SpriteEffects.None, 0);
 	
-				//Plain
-				spriteBatch.Draw(texture, new Vector2(5, 5), new Rectangle(0, 0, (int)size.X, (int)size.Y), Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
-	
-				//2x across
-				spriteBatch.Draw(texture, new Vector2(5, 10 + size.Y), new Rectangle(0, 0, (int)size.X * 2, (int)size.Y), Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
-	
-				//2x down
-				spriteBatch.Draw(texture, new Vector2(5, 15 + 2 *size.Y), new Rectangle(0, 0, (int)size.X, (int)size.Y * 2), Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
-	
-				//2x2 down
-				spriteBatch.Draw(texture, new Vector2(10 + size.X, 15 + 2 * size.Y), new Rectangle(0, 0, (int)size.X * 2, (int)size.Y * 2), Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
-	
-				//1.5 across
-				spriteBatch.Draw(texture, new Vector2(10 + size.X, 5), new Rectangle(0, 0, (int)(size.X * 1.5f), (int)size.Y), Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
-	
-				//1.5 down
-				spriteBatch.Draw(texture, new Vector2(15 + 2.5f * size.X, 5), new Rectangle(0, 0, (int)size.X, (int)(size.Y * 1.5f)), Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
-	
-				//1.5x1.5
-				spriteBatch.Draw(texture, new Vector2(20 + 3.5f * size.X, 5), new Rectangle(0, 0, (int)(size.X * 1.5f), (int)(size.Y * 1.5f)), Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
-	
-	
-				spriteBatch.End();
-			}
-			// TODO: Add your drawing code here
+			spriteBatch.End();
 
 			base.Draw(gameTime);
-			
-			byte[] screenData = new byte[GraphicsDevice.Viewport.Width * GraphicsDevice.Viewport.Height * 4];
-			GraphicsDevice.GetBackBufferData(screenData);
-			//This returns the screen in BGRA format in XNA
-
-#if WINDOWS
-#if !(WINDOWS && DIRECTX)
-			//In XNA and MonoGame.OpenGL we need to swap the R and B bytes so we are in RGBA
-			for (var i = 0; i < screenData.Length; i += 4)
-			{
-				byte temp = screenData[i];
-				screenData[i] = screenData[i + 2];
-				screenData[i + 2] = temp;
-			}
-#endif
-
-			using (var bitmap = new Bitmap(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height))
-			{
-				var data = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb); //This is actually RGBA byte format
-
-				Marshal.Copy(screenData, 0, data.Scan0, screenData.Length);
-
-				bitmap.UnlockBits(data);
-
-				bitmap.Save("out.png", ImageFormat.Png);
-			}
-#endif
-
-#if IOS
-			using (var colorSpace = CGColorSpace.CreateDeviceRGB())
-			using (var provider = new CGDataProvider(screenData, 0, screenData.Length))
-			using (var cgImage = new CGImage(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, 8, 4 * 8, 4 * GraphicsDevice.Viewport.Width, colorSpace, CGBitmapFlags.ByteOrderDefault, provider, null, false, CGColorRenderingIntent.Default))
-			using (var image = UIImage.FromImage(cgImage))
-			{
-				image.SaveToPhotosAlbum(null);
-			}
-#endif
-
-#if ANDROID
-			using (var buffer = ByteBuffer.Wrap(screenData))
-			using (var bitmap = Bitmap.CreateBitmap(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, Bitmap.Config.Argb8888))
-			{
-				bitmap.CopyPixelsFromBuffer(buffer);
-
-				var filename = System.IO.Path.Combine(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath, "out.png");
-
-				//Make sure you have <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
-				using (var output = File.OpenWrite(filename))
-				{
-					bitmap.Compress(Bitmap.CompressFormat.Png, 100, output);
-				}
-			}
-#endif
 		}
 	}
 }
